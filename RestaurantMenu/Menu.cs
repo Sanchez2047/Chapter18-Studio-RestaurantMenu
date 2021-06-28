9bea5ae90ea31f5325dd655908a701fbe33ca7ea
Mike <mike.sierra.7950@gmail.com>
2021-06-28 17:45:10 -0400

    FINAL

---
+++ RestaurantMenu/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_18_Studio
+{
+    static class Menu
+    {
+
+        public static List<MenuItem> MenuItems { get; set; } = new List<MenuItem>();
+
+
+
+
+
+    }
+}
---
+++ RestaurantMenu/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Chapter_18_Studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MenuItem pizza = new MenuItem();
+
+            pizza.Price = 11;
+            pizza.Description = "Pizza";
+            pizza.Category = "Main Course";
+            pizza.DateAdded = new DateTime(2021, 06, 22);
+            pizza.ItemIsNew = pizza.IsNew();
+
+            Menu.MenuItems.Add(pizza);
+
+            MenuItem steak = new MenuItem();
+
+            steak.Price = 35;
+            steak.Description = "Steak: Medium rare deliciousness";
+            steak.Category = "Main Course";
+            steak.DateAdded = new DateTime(2021, 06, 22);
+            steak.ItemIsNew = pizza.IsNew();
+
+
+
+            Menu.MenuItems.Add(steak);
+
+
+            foreach (var menuItem in Menu.MenuItems)
+            {
+                Console.WriteLine($"${menuItem.Price}: {menuItem.Description}: {menuItem.ItemIsNew}");
+
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
